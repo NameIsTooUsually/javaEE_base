f0ebe34714c2e0b5344dabfae596bf2bc7cde25e
NameIsTooUsually <103022861+NameIsTooUsually@users.noreply.github.com>
2022-04-27 17:32:28 +0800

    Update Demo3.java
    测试远程库代码同步到idea

--- 2022-4-27/src/com/chenhao/gitDemo/Demo3.java
+++ 2022-4-27/src/com/chenhao/gitDemo/Demo3.java
@@ -4,5 +4,6 @@ public class Demo3 {
     public static void main(String[] args) {
         System.out.println("修改代码");
         System.out.println("测试是否能成功上传到GitHub");
+        System.out.println("测试拉取远程库");
     }
 }
