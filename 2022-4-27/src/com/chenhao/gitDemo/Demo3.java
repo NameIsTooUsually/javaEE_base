5c5ae77022bfe29225f1f04162bbe917234b82a5
chenhao <852355361@qq.com>
2022-04-27 17:25:48 +0800

    测试

--- 2022-4-27/src/com/chenhao/gitDemo/Demo3.java
+++ 2022-4-27/src/com/chenhao/gitDemo/Demo3.java
@@ -3,5 +3,6 @@ package com.chenhao.gitDemo;
 public class Demo3 {
     public static void main(String[] args) {
         System.out.println("修改代码");
+        System.out.println("测试是否能成功上传到GitHub");
     }
 }
